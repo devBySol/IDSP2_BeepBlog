037046bce219701ba8047027547449a7db6f803d
Sol <ykim327@my.bcit.ca>
2024-10-15 20:06:04 -0700

    Update logo

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -3,13 +3,12 @@ import { EXAMPLE_PATH } from "@/lib/constants";
 
 export function Footer() {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
+    <footer className="bg-[#cdcdcd] border-t border-neutral-200 dark:bg-slate-800">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          {/* <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">Welcome to Beep</h3> */}
-          <img src="/favicon/logo.png" alt="logo" width={200} height={200}></img>
+        <div className="py-28 flex flex-col lg:flex-row items-center mb-1">
+          <img src="/favicon/logo.svg" alt="logo" width={200} height={200} className="lg:mr-6 mb-6 lg:mb-0" />
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a href={`https://github.com/devBySol/IDSP2_BeepBlog/branches`} className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0">
+            <a href={`https://github.com/devBySol/IDSP2_BeepBlog/branches`} className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors ease-in-out mb-6 lg:mb-0 rounded-full shadow-lg hover:shadow-md">
               View on GitHub
             </a>
           </div>
--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -3,7 +3,10 @@ import { CMS_NAME } from "@/lib/constants";
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">Beep</h1>
+      <div className="flex items-center">
+        <img src="/favicon/mainLogo.svg" alt="Beep Logo" className="w-12 h-12 mr-4" />
+        <h1 className="text-5xl md:text-7xl font-bold tracking-tighter leading-tight">Beep</h1>
+      </div>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">Welcome to Beep.</h4>
     </section>
   );
